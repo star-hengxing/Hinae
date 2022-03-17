ed895b3b33cbbb14f1e5be3ea42cebb05942c59c
star-hengxing <hengxings783@gmail.com>
2022-03-17 16:29:06 +0800

    add local and world transform

--- include/Hinae/coordinate_system.hpp
+++ include/Hinae/coordinate_system.hpp
@@ -42,4 +42,38 @@ constexpr Point3<T> spherical_to_cartesian(const Point3<T>& p)
     };
 }
 
+template <arithmetic T>
+constexpr Vector3<T> local_to_world(
+    const Vector3<T>& t, const Vector3<T>& b, const Vector3<T>& n, const Vector3<T>& dir)
+{
+    const auto [x, y, z] = dir;
+    return
+    {
+        t.x * x + b.x * y + n.x * z,
+        t.y * x + b.y * y + n.y * z,
+        t.z * x + b.z * y + n.z * z
+    };
+}
+
+template <arithmetic T>
+constexpr Vector3<T> local_to_world(const Vector3<T>& n, const Vector3<T>& dir)
+{
+    const auto [t, b] = local_coordinate_system(n);
+    return local_to_world(t, b, n, dir);
+}
+
+template <arithmetic T>
+constexpr Vector3<T> world_to_local(
+    const Vector3<T>& t, const Vector3<T>& b, const Vector3<T>& n, const Vector3<T>& dir)
+{
+    return {dot(dir, t), dot(dir, b), dot(dir, n)};
+}
+
+template <arithmetic T>
+constexpr Vector3<T> world_to_local(const Vector3<T>& n, const Vector3<T>& dir)
+{
+    const auto [t, b] = local_coordinate_system(n);
+    return world_to_local(t, b, n, dir);
+}
+
 NAMESPACE_END(Hinae)
