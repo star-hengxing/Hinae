b4b1266364f415896cdfd19fbfcd7bd05b494c90
star-hengxing <hengxings783@gmail.com>
2022-03-28 23:37:13 +0800

    add easy to compute cosine function

--- include/Hinae/Trigonometric.hpp
+++ include/Hinae/Trigonometric.hpp
@@ -40,4 +40,76 @@ constexpr T cos_to_sin(T cos)
     return std::sqrt(cos_to_sin2(cos));
 }
 
+NAMESPACE_BEGIN(Local)
+
+template <arithmetic T>
+constexpr T cos_theta(const Vector3<T>& v)
+{
+    return v.z;
+}
+
+template <arithmetic T>
+constexpr T cos2_theta(const Vector3<T>& v)
+{
+    return cos_theta(v) * cos_theta(v);
+}
+
+template <arithmetic T>
+constexpr T abs_cos_theta(const Vector3<T>& v)
+{
+    return std::abs(cos_theta(v));
+}
+
+template <arithmetic T>
+constexpr T sin_theta(const Vector3<T>& v)
+{
+    return cos_to_sin(cos_theta(v));
+}
+
+template <arithmetic T>
+constexpr T sin2_theta(const Vector3<T>& v)
+{
+    return cos_to_sin2(cos_theta(v));
+}
+
+template <arithmetic T>
+constexpr T tan_theta(const Vector3<T>& v)
+{
+    return sin_theta(v) / cos_theta(v);
+}
+
+template <arithmetic T>
+constexpr T tan2_theta(const Vector3<T>& v)
+{
+    return sin2_theta(v) / cos2_theta(v);
+}
+
+template <arithmetic T>
+constexpr T cos_phi(const Vector3<T>& v)
+{
+    const T sin = sin_theta(v);
+    return is_zero(sin) ? 1 : clamp(-ONE<T>, v.x / sin, ONE<T>);
+}
+
+template <arithmetic T>
+constexpr T sin_phi(const Vector3<T>& v)
+{
+    const T sin = sin_theta(v);
+    return is_zero(sin) ? 1 : clamp(-ONE<T>, v.y / sin, ONE<T>);
+}
+
+template <arithmetic T>
+constexpr T cos2_phi(const Vector3<T>& v)
+{
+    return cos_phi(v) * cos_phi(v);
+}
+
+template <arithmetic T>
+constexpr T sin2_phi(const Vector3<T>& v)
+{
+    return sin_phi(v) * sin_phi(v);
+}
+
+NAMESPACE_END(Local)
+
 NAMESPACE_END(Hinae)
