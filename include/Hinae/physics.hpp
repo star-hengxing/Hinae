5d7bdc89e55e857129f5a78786f75cc04903538c
star-hengxing <hengxings783@gmail.com>
2022-04-21 19:57:33 +0800

    fix bug and clean code

--- include/Hinae/physics.hpp
+++ include/Hinae/physics.hpp
@@ -1,6 +1,7 @@
 #pragma once
 
 #include <optional>
+#include <complex>
 
 #include "Trigonometric.hpp"
 
@@ -23,24 +24,27 @@ refract(const Vector3<T>& wi, const Vector3<T>& n, T eta)
     if(sin2_theta_t >= 1) return {};
 
     const T cos_theta_t = sin2_to_cos(sin2_theta_t);
-    return Vector3(eta * wi - (eta * cos_theta_i - cos_theta_t) * n);
+    return Vector3(eta * wi - (eta * cos_theta_i + cos_theta_t) * n);
 }
 
 NAMESPACE_BEGIN(Fresnel)
 
-template <arithmetic T>
+template <bool safe = true, arithmetic T>
 constexpr T dielectric(T cos_theta_i, T eta_i, T eta_t)
 {
-    cos_theta_i = clamp(-ONE<T>, cos_theta_i, ONE<T>);
-    if(cos_theta_i < 0)
+    if constexpr(safe)
     {
-        std::swap(eta_i, eta_t);
-        cos_theta_i = std::abs(cos_theta_i); // cos = -cos
+        if(cos_theta_i < 0)
+        {
+            std::swap(eta_i, eta_t);
+            cos_theta_i = -cos_theta_i;
+        }
     }
 
     T sin_theta_i = sin_to_cos(cos_theta_i);
     T sin_theta_t = eta_i / eta_t * sin_theta_i;
     if(sin_theta_t >= 1) return 1;
+
     T cos_theta_t = sin_to_cos(sin_theta_t);
 
     T r_parl = ((eta_t * cos_theta_i) - (eta_i * cos_theta_t))
@@ -52,34 +56,30 @@ constexpr T dielectric(T cos_theta_i, T eta_i, T eta_t)
     return (pow2(r_parl) + pow2(r_perp)) / 2;
 }
 
-template <arithmetic T>
-constexpr T conductor(
-    T cos_theta_i,
-    const Vector3<T>& eta_t,
-    const Vector3<T>& eta_i,
-    const Vector3<T>& k)
+template <bool safe = true, arithmetic T>
+constexpr T conductor(T cos_theta_i, std::complex<T> eta_i, std::complex<T> eta_t)
 {
-    cos_theta_i = clamp(-ONE<T>, cos_theta_i, ONE<T>);
-    Vector3<T> eta = eta_t / eta_i;
-    Vector3<T> eta_k = k / eta_i;
+    if constexpr(safe)
+    {
+        if(cos_theta_i < 0)
+        {
+            std::swap(eta_i, eta_t);
+            cos_theta_i = -cos_theta_i;
+        }
+    }
 
-    T cos2_theta_i = cos_theta_i * cos_theta_i;
-    T sin2_theta = 1 - cos2_theta_i;
-    Vector3<T> eta2 = eta * eta;
-    Vector3<T> eta_k2 = eta_k * eta_k;
+    T sin_theta_i = std::sqrt(ONE<T> - cos_theta_i * cos_theta_i);
+    auto sin_theta_t = eta_i / eta_t * sin_theta_i;
 
-    Vector3<T> t0 = eta2 - eta_k2 - sin2_theta;
-    Vector3<T> a2_plus_b2 = Hinae::sqrt(t0 * t0 + 4 * eta2 * eta_k2);
-    Vector3<T> t1 = a2_plus_b2 + cos2_theta_i;
-    Vector3<T> a  = Hinae::sqrt(0.5f * (a2_plus_b2 + t0));
-    Vector3<T> t2 = 2 * a * cos_theta_i;
-    Vector3<T> Rs = (t1 - t2) / (t1 + t2);
+    auto cos_theta_t = std::sqrt(ONE<T> - sin_theta_t * sin_theta_t);
 
-    Vector3<T> t3 = cos2_theta_i * a2_plus_b2 + sin2_theta * sin2_theta;
-    Vector3<T> t4 = t2 * sin2_theta;   
-    Vector3<T> Rp = Rs * (t3 - t4) / (t3 + t4);
+    auto r_parl = ((eta_t * cos_theta_i) - (eta_i * cos_theta_t))
+                / ((eta_t * cos_theta_i) + (eta_i * cos_theta_t));
 
-    return (Rp + Rs) / 2;
+    auto r_perp = ((eta_i * cos_theta_i) - (eta_t * cos_theta_t))
+                / ((eta_i * cos_theta_i) + (eta_t * cos_theta_t));
+
+    return (std::norm(r_parl) + std::norm(r_perp)) / 2;
 }
 
 NAMESPACE_END(Fresnel)
